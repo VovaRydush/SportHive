a20d758df189b6bc436b8a50636c605375284a83
VovaRydush <vadimrudis8@gmail.com>
2025-04-03 22:15:43 +0300

    add swagger

--- Services/AuthService/AuthService.cs
+++ Services/AuthService/AuthService.cs
@@ -7,16 +7,21 @@ using StackExchange.Redis;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
 builder.Services.AddDbContext<AppDbContext>(options => options.UseNpgsql(builder.Configuration.GetConnectionString("Primary")));
 builder.Services.AddSingleton<IConnectionMultiplexer>(ConnectionMultiplexer.Connect("localhost:6379"));
 
 builder.Services.AddScoped<IRedisService, RedisService>();
 builder.Services.AddScoped<IUserService, UserService>();
 
-
-
-
 var app = builder.Build();
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
 
 app.MapAuthEndpoints();
 
